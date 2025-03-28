332fca70e22b3f4359e4a122f5ecb335246a8a97
Justin <lujust@umich.edu>
2025-03-28 19:25:08 -0400

    removed dependencies related to legacy robots and registered new go2 rnn task

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -34,14 +34,13 @@ from .base.legged_robot import LeggedRobot
 from .anymal_c.anymal import Anymal
 from .anymal_c.mixed_terrains.anymal_c_rough_config import AnymalCRoughCfg, AnymalCRoughCfgPPO
 from .anymal_c.flat.anymal_c_flat_config import AnymalCFlatCfg, AnymalCFlatCfgPPO
-from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
 from .cassie.cassie import Cassie
 from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 
 from .go2.go2 import Go2Robot
 from .go2.go2_config import Go2Cfg, Go2CfgPPO
-from .go2.go2_stand_config import Go2StandCfg, Go2StandCfgPPO
+from .go2.go2_rnn_config import Go2RnnCfg, Go2RnnCfgPPO
 
 
 import os
@@ -50,8 +49,7 @@ from legged_gym.utils.task_registry import task_registry
 
 task_registry.register( "anymal_c_rough", Anymal, AnymalCRoughCfg(), AnymalCRoughCfgPPO() )
 task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCfgPPO() )
-task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
 task_registry.register( "go2", Go2Robot, Go2Cfg(), Go2CfgPPO() )
-task_registry.register( "go2_stand", Go2Robot, Go2StandCfg(), Go2StandCfgPPO() )
+task_registry.register( "go2_rnn", Go2Robot, Go2RnnCfg(), Go2RnnCfgPPO() )
